c70981c33c3b036e101b29eb5b36098b7bb9268f
ILYA <ily.kolot21@gmail.com>
2025-01-15 12:00:02 +0700

    create first actions

---
+++ app/api/actions/donate/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  'HaiC2fVwPt5ZhoCq8PYsqmUYW87u6ac3yPx1xYwChCSs'
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
